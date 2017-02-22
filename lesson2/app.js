29c021fa327cbf0b5a2464486aa6dff93869c17a
zhangy <yonggao.zhang@vivame.cn>
2017-02-22 18:00:28 +0800

    learning

---
+++ lesson2/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var utility = require('utility');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    var q = req.query.q;
+
+    // var md5Value = utility.md5(q);
+    //
+    // res.send(md5Value);
+
+    var sha1Value = utility.sha1(q);
+
+    res.send(sha1Value);
+});
+
+app.listen(3000, function () {
+    console.log('app is at running at port 3000')
+});
